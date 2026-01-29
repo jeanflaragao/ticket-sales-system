5a6dfc6c1a98d8bf4b6035e23ad8ae215ad56625
Jean Aragão <aragao.jean@gmail.com>
2026-01-28 21:17:23 -0300

    chore: Remove unused test worker files
    - Delete test_worker.rb and test_worker_2.rb (not used in application)
    - Clean up worker directory structure
    - Remove unused background job components

--- app/workers/test_worker.rb
+++
@@ -1,8 +0,0 @@
-class TestWorker
-  include Sidekiq::Worker
-  
-  def perform(name)
-    puts "Hello from Sidekiq, #{name}!"
-    Rails.logger.info("TestWorker executed for #{name}")
-  end
-end
--- app/workers/test_worker_2.rb
+++
@@ -1,8 +0,0 @@
-class TestWorker2
-  include Sidekiq::Worker
-  
-  def perform(name)
-    puts "Hello from Sidekiq 2, #{name}!"
-    Rails.logger.info("TestWorker 2 executed for #{name}")
-  end
-end
